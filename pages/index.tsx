9ccb280339fbd8d71082d7f5b34211a057ee744d
demo308 <76916261+demo308@users.noreply.github.com>
2023-12-03 16:17:38 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,26 @@ function HomePage() {
       <Container>
         <div className="space-y-6">
           <h1 className="text-2xl font-bold">
-            Hey test site
+            Hey test sute
           </h1>
-         
+          <p>
+         test site
+          </p>
+
+          <p>Depolot</p>
+        </div>
+      </Container>
+
+      <div className="container max-w-4xl m-auto px-4 mt-20">
+        <Image
+          src="/desk.jpg"
+          alt="my desk"
+          width={1920 / 2}
+          height={1280 / 2}
+        />
+      </div>
+    </>
+  )
+}
+
+export default HomePage
